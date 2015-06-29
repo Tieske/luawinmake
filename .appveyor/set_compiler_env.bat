c03216a09fa21077f99c115c60b4c466392bca36
Ignacio Burgueño <ignaciob@inconcertcc.com>
2015-06-29 14:35:37 -0300

    Adds MinGW to the build matrix.
    Only mingw32 is installed, so the { mingw, x64 } configuration is a farce.
    However, I think it is better to include it anyway.

--- .appveyor/set_compiler_env.bat
+++ .appveyor/set_compiler_env.bat
@@ -4,11 +4,12 @@
 Setlocal EnableDelayedExpansion EnableExtensions
 
 if not defined Configuration set Configuration=2015
+
+if "%Configuration%"=="MinGW" ( goto :mingw )
+
 set arch=x86
 
-if "%platform%" EQU "x64" (
-	set arch=x86_amd64
-)
+if "%platform%" EQU "x64" ( set arch=x86_amd64 )
 
 if "%Configuration%"=="2015" (
 	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
@@ -30,4 +31,10 @@ if "%Configuration%"=="2008" (
 	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 )
 
+:: Visual Studio detected
 endlocal & call %SET_VS_ENV% %arch%
+goto :eof
+
+:: MinGW detected
+:mingw
+endlocal & set PATH=c:\mingw\bin;%PATH%
