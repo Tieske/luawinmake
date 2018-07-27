52fca218668e5ef731e698a650e4ce13c5a6bd24
Ignacio Burgueno <ignacio.burgueno@king.com>
2018-07-27 18:32:20 +0200

    Adds support for VS 2017 and mingw-w64
    Adds Visual Studio 2017 to the compilation matrix.
    Starts using mingw-w64 7.3.0 x86_64 instead of MinGW 32-bit 5.3.0

--- .appveyor/set_compiler_env.bat
+++ .appveyor/set_compiler_env.bat
@@ -3,13 +3,17 @@
 :: Now we declare a scope
 Setlocal EnableDelayedExpansion EnableExtensions
 
-if not defined Configuration set Configuration=2015
+if not defined Configuration set Configuration=2017
 
 if "%Configuration%"=="MinGW" ( goto :mingw )
 
 set arch=x86
 
 if "%platform%" EQU "x64" ( set arch=x86_amd64 )
+
+if "%Configuration%"=="2017" (
+	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvarsall.bat"
+)
 
 if "%Configuration%"=="2015" (
 	set SET_VS_ENV="C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
@@ -37,4 +41,4 @@ goto :eof
 
 :: MinGW detected
 :mingw
-endlocal & set PATH=c:\mingw\bin;%PATH%
+endlocal & set PATH=C:\mingw-w64\x86_64-7.3.0-posix-seh-rt_v5-rev0\mingw64\bin;%PATH%
