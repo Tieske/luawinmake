2c235d36c79ac1d01af3e214def30cb748425684
Thijs Schreijer | Vcare <t.schreijer@vcareconnect.nl>
2025-12-27 11:32:48 +0100

    minor comment update

--- etc/winmake.bat
+++ etc/winmake.bat
@@ -218,7 +218,7 @@ if %ERRORLEVEL%==0 (
 rem Check on a Lua version 5.2.x to 5.4.x
 findstr /R /C:"#define[ %TABCHAR%][ %TABCHAR%]*LUA_VERSION_MAJOR[ %TABCHAR%]"  %LUA_H% > NUL
 if %ERRORLEVEL%==0 (
-   rem ECHO We've got a Lua version 5.2.0 to 5.4.6
+   rem ECHO We've got a Lua version 5.2.x to 5.4.x
    rem findstr /R /C:"#define[ %TABCHAR%][ %TABCHAR%]*LUA_VERSION_MAJOR[ %TABCHAR%]"  %LUA_H%
    rem findstr /R /C:"#define[ %TABCHAR%][ %TABCHAR%]*LUA_VERSION_MINOR[ %TABCHAR%]"  %LUA_H%
 
