c1e1832cf3d8354b486a109f276665aab1004a64
Thijs Schreijer | Vcare <t.schreijer@vcareconnect.nl>
2025-12-27 09:23:06 +0100

    update to release 5.5

--- etc/winmake.bat
+++ etc/winmake.bat
@@ -14,6 +14,7 @@ REM set the compatibility flags, defaults to empty for 5.1,
 REM -DLUA_COMPAT_ALL for 5.2, 
 REM -DLUA_COMPAT_5_2 for 5.3,
 REM -DLUA_COMPAT_5_3 for 5.4,
+REM -DLUA_COMPAT_5_4 for 5.5,
 REM which are the same as the unix make files
 REM This setting can be overridden with the --nocompat flag
 SET COMPATFLAG=
@@ -78,7 +79,7 @@ SET HELPCMDS=help -help --help /help ? -? /?
 for %%L in ("!LFCHAR!") do for /f %%a in ("!HELPCMDS: =%%~L!") do (
    if "%%a"=="%~1" (
       echo.
-      echo Builds a standalone Lua installation. Supports Lua version 5.1, 5.2, 5.3, and 5.4.
+      echo Builds a standalone Lua installation. Supports Lua version 5.1, 5.2, 5.3, 5.4, and 5.5.
       echo Your compiler must be in the system path, and this "%BATCHNAME%.bat" file
       echo should ^(preferably^) be located in ".\etc\" in the unpacked Lua source archive.
       echo.
@@ -190,7 +191,7 @@ if NOT %ERRORLEVEL%==0 (
 rem Check on a Lua 5.5+ version
 findstr /R /C:"#define[ %TABCHAR%][ %TABCHAR%]*LUA_VERSION_MAJOR_N[ %TABCHAR%]"  %LUA_H% > NUL
 if %ERRORLEVEL%==0 (
-   rem ECHO We've got a Lua version 5.4.7+
+   rem ECHO We've got a Lua version 5.5+
    rem findstr /R /C:"#define[ %TABCHAR%][ %TABCHAR%]*LUA_VERSION_MAJOR_N[ %TABCHAR%]"  %LUA_H%
    rem findstr /R /C:"#define[ %TABCHAR%][ %TABCHAR%]*LUA_VERSION_MINOR_N[ %TABCHAR%]"  %LUA_H%
 
@@ -297,6 +298,16 @@ if %LUA_SVER%==54 (
    set INSTALL_H=lauxlib.h lua.h lua.hpp luaconf.h lualib.h
    if "%COMPATFLAG%"=="" (
       set COMPATFLAG=-DLUA_COMPAT_5_3
+   )
+)
+if %LUA_SVER%==55 (
+   set FILES_CORE=lapi lcode lctype ldebug ldo ldump lfunc lgc llex lmem lobject lopcodes lparser lstate lstring ltable ltm lundump lvm lzio lauxlib
+   set FILES_LIB=lbaselib lcorolib ldblib liolib lmathlib loadlib loslib lstrlib ltablib lutf8lib linit
+   set FILES_DLL=lua
+   set FILES_OTH=luac
+   set INSTALL_H=lauxlib.h lua.h lua.hpp luaconf.h lualib.h
+   if "%COMPATFLAG%"=="" (
+      set COMPATFLAG=-DLUA_COMPAT_5_4
    )
 )
 
